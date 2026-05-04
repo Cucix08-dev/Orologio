fc3bd06c6256aba70bcdc211dc57736433732b45
Cucix08-dev <cucixdev@gmail.com>
2026-05-04 15:15:31 +0200

    Added style and fonts

---
+++ clock.js
@@ -0,0 +1,41 @@
+//VARS
+const clocks = ["Digital", "Analog", "Roman", "Math", "Programmer"]
+let clockSelectedIndex = 0;
+let time = new Date();
+let hours = time.getHours();
+let minutes = time.getMinutes();
+let seconds = time.getSeconds();
+
+// CONTAINERS
+const clockSelectedContainer = document.getElementById("clock-selected");
+clockSelectedContainer.textContent = clocks[clockSelectedIndex];
+const body = document.body;
+body.classList.add("digital");
+
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+leftArrow.addEventListener("click", () => {
+    if (clockSelectedIndex-1<0) {
+        clockSelectedIndex = clocks.length-1;
+        clockSelectedContainer.textContent = clocks[clockSelectedIndex];
+    }
+    else{
+        clockSelectedContainer.textContent = clocks[--clockSelectedIndex];
+    }
+    body.style.fontFamily = clocks[clockSelectedIndex].toLowerCase();
+});
+
+rightArrow.addEventListener("click", () => {
+    if (clockSelectedIndex+1>clocks.length-1) {
+        clockSelectedIndex = 0;
+        clockSelectedContainer.textContent = clocks[clockSelectedIndex];
+    }
+    else{
+        clockSelectedContainer.textContent = clocks[++clockSelectedIndex];
+    }
+    body.style.fontFamily = clocks[clockSelectedIndex].toLowerCase();
+});
+
+
+
